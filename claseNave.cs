b965b712d7f6db80f6fa90acda15dcb706507831
win 10 <win 10@DESKTOP-PF10VV7>
2024-04-07 23:59:51 -0300

    Agregar archivos de proyecto.

---
+++ claseNave.cs
@@ -0,0 +1,39 @@
+﻿using NPoco.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmJuego
+{
+    internal class claseNave
+    {
+        //carecteristicas de las naves
+        int vida;
+        string jugador;
+        int daño;
+        public PictureBox imagNave;
+        public void CrearJugador()
+        {
+            vida = 100;
+            jugador = "player";
+            daño = 1;
+            imagNave = new PictureBox();
+            imagNave.SizeMode = PictureBoxSizeMode.StretchImage;
+            imagNave.ImageLocation = "https://www.kindpng.com/picc/m/210-2102031_galaga-ship-no-background-png-download-8-bit.png";
+
+
+        }
+        public void CrearEnemigo()
+        {
+            vida = 1;
+            jugador = "enemigo1";
+            daño = 25;
+
+
+        }
+
+    }
+}
---
+++ frmJuego.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmJuego
+{
+    public partial class frmJuego : Form
+    {
+        claseNave objNaveJugador;
+        public frmJuego()
+        {
+            InitializeComponent();
+        }
+        int x = 100;
+        int y = 100;
+        private void frmJuego_Load(object sender, EventArgs e)
+        {
+            objNaveJugador = new claseNave();
+            objNaveJugador.CrearJugador();
+            objNaveJugador.CrearEnemigo();
+            Controls.Add(objNaveJugador.imagNave);
+            objNaveJugador.imagNave.Location = new Point(x, y);
+        }
+
+        private void frmJuego_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                x = x + 25;
+                objNaveJugador.imagNave.Location = new Point(x, y);
+            }
+            if(e.KeyCode == Keys.Left)
+            {
+                x = x - 25;
+                objNaveJugador.imagNave.Location = new Point(x, y);
+            }
+
+        }
+    }
+}
